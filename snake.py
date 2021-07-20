743ed6909257a2ae8133d6803f54a13521c94eda
geraldinemulimi <geraldinemulimi@gmail.com>
2021-07-20 21:00:58 +0300

    modified snakegame

--- snake.py
+++ snake.py
@@ -24,6 +24,13 @@ class Snake:
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
+        
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def extend(self):
         # add a new segment to the snake
